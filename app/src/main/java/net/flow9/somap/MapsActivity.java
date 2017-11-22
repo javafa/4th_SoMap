231cf19163b49d4ae027053226cfa7a1714595fa
javafa <nigne2016@gmail.com>
2017-11-22 16:25:24 +0900

    add car list

--- app/src/main/java/net/flow9/somap/MapsActivity.java
+++ app/src/main/java/net/flow9/somap/MapsActivity.java
@@ -2,6 +2,8 @@ package net.flow9.somap;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.Toast;
@@ -16,6 +18,7 @@ import com.google.maps.android.clustering.Cluster;
 import com.google.maps.android.clustering.ClusterItem;
 import com.google.maps.android.clustering.ClusterManager;
 
+import net.flow9.somap.domain.CarApi;
 import net.flow9.somap.domain.Data;
 import net.flow9.somap.domain.ZoneApi;
 
@@ -34,7 +37,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
     private ProgressBar progressBar;
+    private ProgressBar progressBar2;
     private ClusterManager<MarkerItem> clusterManager;
+
+    private RecyclerView recyclerView;
+    private CustomAdapter adapter;
 
     private Map<String, LatLng> citys = new HashMap<>();
 
@@ -42,12 +49,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        // 맵 세팅
         progressBar = findViewById(R.id.progressBar);
         progressBar.setVisibility(View.VISIBLE);
-
         citys.put("서울",new LatLng(37.516038, 127.019783));
         citys.put("부산",new LatLng(35.168359, 129.176855));
         citys.put("광주",new LatLng(35.154507, 126.845171));
+
+        // 리스트 세팅
+        progressBar2 = findViewById(R.id.progressBar2);
+        recyclerView = findViewById(R.id.recyclerView);
+        adapter = new CustomAdapter();
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         try{
             ZoneApi.getZones(this);
@@ -81,11 +95,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         });
 
-        // * 단일 마커 클릭 시 처리 : 상세보기로 이동 등의 처리
+        // * 단일 마커 클릭 시 처리 : 아래 목록에 자동차 리스팅
         clusterManager.setOnClusterItemClickListener(new ClusterManager.OnClusterItemClickListener<MarkerItem>() {
             @Override
             public boolean onClusterItemClick(MarkerItem markerItem) {
-
+                setCarList(markerItem.zone_id);
                 return false;
             }
         });
@@ -96,6 +110,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         setClusterOnMap();
     }
 
+    /**
+     * 마커 클릭시 자동차 목록 갱신하기
+     * @param zone_id
+     */
+    private void setCarList(String zone_id){
+        try {
+            progressBar2.setVisibility(View.VISIBLE);
+            CarApi.getCars(zone_id, () -> {
+                adapter.setDataAndRefresh(CarApi.data);
+                progressBar2.setVisibility(View.GONE);
+            });
+        }catch(Exception e){
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    /**
+     * 지도에 마커 클러스터 처리하기
+     */
     private void setClusterOnMap(){
         clusterManager.clearItems();
         Observable<Data> observable = Observable.create(emitter -> {
@@ -109,7 +143,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             .subscribe(data ->{
                 // 2. 클러스터 매니저에 마커를 등록
                 MarkerItem item = new MarkerItem(data.getLat(), data.getLng());
-                item.title = data.getZone_name();
+                item.zone_id = data.getZone_id();
                 clusterManager.addItem(item);
                 // LatLng marker = new LatLng(data.getLat(), data.getLng());
                 // mMap.addMarker(new MarkerOptions().position(marker).title(data.getZone_name()));
@@ -162,7 +196,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     class MarkerItem implements ClusterItem {
         private LatLng position;
-        public String title;
+        public String zone_id;
 
         public MarkerItem(double lat, double lng){
             position = new LatLng(lat, lng);
--- node/routes/car.js
+++ node/routes/car.js
@@ -1,23 +1,35 @@
 var express = require("express")
 var router = express.Router()
 
-// ROOT : /user 읽기
+var dao = require("../dao/carDao");
+
+// ROOT : /car 읽기
 router.get("/",function(req, res, callback){
-    res.send("request user select")
-})
-// /user 쓰기
-router.post("/", function(req,res){
-    res.send("request user write")
-})
-// /user 수정
-router.put("/", function(req,res){
-    res.send("request user update")
-})
-// /user 삭제
-router.delete("/", function(req,res){
-    res.send("request user delete")
+    res.send("404 not found!")
 })
 
+router.get("/:zone_id",function(req, res, callback){
+    var result = {
+        code : "",
+        msg : "",
+        count : "",
+        carData : []
+    };
+
+    dao.selectArea(req.params.zone_id, function(error, items){
+        if(error){
+            result.code = "501";
+            result.msg = error;
+        }else{
+            result.code = "200";
+            result.msg = "success";
+            result.count = items.length;
+            result.carData = items;
+        }
+        res.send(JSON.stringify(result));
+    });
+
+})
 
 // exports를 통해서 외부로부터 위에서 정의된 변수에 접근할 수 있게 해준다
 module.exports = router
